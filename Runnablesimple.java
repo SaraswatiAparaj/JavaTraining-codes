f22cafdfb415566a4d743284d7553c9860ed0156
SaraswatiAparaj <113429660+SaraswatiAparaj@users.noreply.github.com>
2022-10-09 22:30:20 +0530

    Add files via upload

---
+++ Runnablesimple.java
@@ -0,0 +1,27 @@
+package com.pkg;
+
+public class Runnablesimple implements Runnable
+{
+	public void run()
+	{
+		System.out.println("thread"+Thread.currentThread().getName());
+	}
+
+	public static void main(String[] args)
+	{
+		System.out.println("charu"+Thread.currentThread().getName());
+		
+		Runnablesimple ob1=new Runnablesimple();
+		Runnablesimple ob2=new Runnablesimple();
+		
+		//ob1.start();
+		//ob2.start();
+		Thread t1=new Thread(ob2);
+		Thread t2=new Thread(ob1);
+		t1.start();
+		t2.start();
+
+	}
+
+}
+
