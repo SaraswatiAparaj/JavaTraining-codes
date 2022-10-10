fe919336a4c628b1a0903cf7c60fa0788afe6b72
SaraswatiAparaj <113429660+SaraswatiAparaj@users.noreply.github.com>
2022-10-10 18:01:23 +0530

    Add files via upload

---
+++ Synchronizeblock.java
@@ -0,0 +1,65 @@
+package com.pkg;
+
+class synchronizeclas
+{
+	int count=0;
+	
+	synchronized int inc()
+	{
+		System.out.println("with Synchronized");
+		synchronized(this)
+		{
+		return count++;
+		}
+	}
+}
+
+
+public class Synchronizeblock
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+	synchronizeclas obj1 = new synchronizeclas();
+
+		
+		
+		class T1 extends Thread
+		{
+			public void run()
+			{
+				
+				  for(int i=0;i<100;i++)
+				  { obj1.inc(); 
+				  }	
+				
+			}
+		}
+
+
+		class T2 extends Thread 
+		 { 
+			 public void run() 
+			 { 
+				 for(int i=0;i<100;i++) 
+				 {
+					 obj1.inc(); 
+		 } 
+				 }
+		 }
+		
+		T1 ob = new T1();
+	
+		
+		T2 ob1 = new T2();
+		ob.start();
+		ob.join();
+	
+		ob1.start();
+		ob1.join();
+		
+	//	Thread.sleep(1000);
+		System.out.println(obj1.count);
+		
+	}
+}
+
---
+++ Withsynchronized.java
@@ -0,0 +1,64 @@
+package com.pkg;
+
+class synchronizeclas1
+{
+	int count=0;
+	
+	synchronized int inc()
+	{
+		
+		return count++;
+	}
+}
+
+
+public class Withsynchronized
+{
+	public static void main(String[] args) throws InterruptedException 
+	{
+		
+		
+		class T1 extends Thread
+		{
+			synchronizeclas1 obj1 = new synchronizeclas1();
+
+			public void run()
+			{
+				
+				  for(int i=0;i<100;i++)
+				  { obj1.inc(); 
+					System.out.println(obj1.count);
+				  }	
+				
+			}
+		}
+
+
+		class T2 extends Thread 
+		 { 
+			synchronizeclas1 obj2 = new synchronizeclas1();
+			 public void run() 
+			 { 
+				 for(int i=0;i<100;i++) 
+				 {
+					 obj2.inc(); 
+						System.out.println(obj2.count);
+		 } } }
+		 
+		
+		
+		T1 ob = new T1();
+	
+		
+		T2 ob1 = new T2();
+		ob.start();
+		ob.join();
+	
+		ob1.start();
+		ob1.join();
+		
+	//	Thread.sleep(1000);
+		
+	}
+}
+
